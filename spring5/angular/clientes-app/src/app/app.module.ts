8c0331ff835402c5517867d102c0711fb5b9db91
Javier Simbaña <sjavi18_alt@hotmail.com>
2020-10-27 11:37:41 -0500

    JSI - feat: add proyecto spring con CRUD y avance de crud en angular

--- spring5/angular/clientes-app/src/app/app.module.ts
+++ spring5/angular/clientes-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,16 +9,22 @@ import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import { FormsModule } from '@angular/forms';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/clientes', pathMatch: 'full'},
   { path: 'directivas', component: DirectivaComponent },
   { path: 'clientes', component: ClientesComponent },
+  { path: 'clientes/form', component: FormComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule {}
 
@@ -29,10 +36,12 @@ export class AppRoutingModule {}
     FooterComponent,
     DirectivaComponent,
     ClientesComponent,
-
+    FormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
